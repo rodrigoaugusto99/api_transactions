e4061c68bba578739ba0d61b473651008028779d
rodrigoaugusto99 <rodrigoaugusto51@outlook.com>
2024-05-01 14:45:48 -0300

    rodando as migrations + eslint

--- src/server.ts
+++ src/server.ts
@@ -1,33 +1,13 @@
-import fastify from "fastify"
+import fastify from 'fastify'
 
 const app = fastify()
 
 app.get('/hello', () => {
-    return 'Hello World'
+  return 'hi m'
 })
 
-app.listen({
-    port: 3333
-}).then(() => console.log('server is running on port 3333'))
-
-
-/*
-npm init -y
-npm i -D typescript
-npx tsc --init
-npm i fastify
-npm i tsx -D
-npx tsx src/server.ts
-
-git init
-git add .
-git commit -m "initial commit" 
-git remote add origin https://github.com/rodrigoaugusto99/api_rest_node_test.git
-git push -u origin master
-
-npm i knex sqlite3
-knexfile.ts
-@types w/ knex.d.ts
-script "knex": "node --no-warnings --import tsx ./node_modules/knex/bin/cli.js"
-npm run knex -- migrate:make create-documents    
-*/
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => console.log('server is running on port 3333'))
