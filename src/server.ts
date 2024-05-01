5fd8b04abd50ea333859a4929db977f1108436ff
rodrigoaugusto99 <rodrigoaugusto51@outlook.com>
2024-05-01 00:39:53 -0300

    preparando para criar a migration

--- src/server.ts
+++ src/server.ts
@@ -18,4 +18,16 @@ npx tsc --init
 npm i fastify
 npm i tsx -D
 npx tsx src/server.ts
+
+git init
+git add .
+git commit -m "initial commit" 
+git remote add origin https://github.com/rodrigoaugusto99/api_rest_node_test.git
+git push -u origin master
+
+npm i knex sqlite3
+knexfile.ts
+@types w/ knex.d.ts
+script "knex": "node --no-warnings --import tsx ./node_modules/knex/bin/cli.js"
+npm run knex -- migrate:make create-documents    
 */
